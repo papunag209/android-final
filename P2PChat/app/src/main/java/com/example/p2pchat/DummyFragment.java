c76d775152ecb266965d4a37d3f675de6af979c3
papuna gaprindashvili <papunag209@gmail.com>
2019-07-19 23:18:52 +0400

    update to menu items

---
+++ P2PChat/app/src/main/java/com/example/p2pchat/DummyFragment.java
@@ -0,0 +1,31 @@
+package com.example.p2pchat;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DummyFragment extends Fragment {
+
+
+    public DummyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dummy, container, false);
+    }
+
+}
