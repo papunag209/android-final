8909935f02edc41667b9aa3e7a981df9e522edbb
papuna gaprindashvili <papunag209@gmail.com>
2019-07-24 16:02:42 +0400

    Data Models Implemented for Message and Session

--- P2PChat/app/src/main/java/com/example/p2pchat/data/model/Message.java
+++ P2PChat/app/src/main/java/com/example/p2pchat/data/model/Message.java
@@ -1,10 +1,25 @@
 package com.example.p2pchat.data.model;
 
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity
+import java.sql.Time;
+
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(foreignKeys = @ForeignKey(  entity = Session.class,
+        parentColumns = "SessionId",
+        childColumns = "SessionId",
+        onDelete = CASCADE
+))
 public class Message {
     @PrimaryKey(autoGenerate = true)
     Long MessageId;
+
+    Long SessionId;
+
+    Time MessageTime;
+
+    String MessageStatus;
 }
--- P2PChat/app/src/main/java/com/example/p2pchat/data/model/Session.java
+++ P2PChat/app/src/main/java/com/example/p2pchat/data/model/Session.java
@@ -3,9 +3,15 @@ package com.example.p2pchat.data.model;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.sql.Time;
+
 @Entity
 public class Session {
 
     @PrimaryKey(autoGenerate = true)
     Long SessionId;
+
+    Time SessionStartTime;
+
+    String PeerPhoneName;
 }
