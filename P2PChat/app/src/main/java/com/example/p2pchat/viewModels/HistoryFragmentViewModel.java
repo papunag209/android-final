9aef1b620919b1a0b6492e2b707bd9792e2f8645
misho <mmetr14@freeuni.edu.ge>
2019-08-06 23:41:37 +0400

    change from rxjava to asynctask

--- P2PChat/app/src/main/java/com/example/p2pchat/viewModels/HistoryFragmentViewModel.java
+++ P2PChat/app/src/main/java/com/example/p2pchat/viewModels/HistoryFragmentViewModel.java
@@ -1,10 +1,13 @@
 package com.example.p2pchat.viewModels;
 
+import android.os.AsyncTask;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.p2pchat.data.DataDao;
 import com.example.p2pchat.data.Database;
+import com.example.p2pchat.data.model.Message;
 import com.example.p2pchat.data.model.Session;
 import com.example.p2pchat.data.model.helperModel.SessionWithMessageCount;
 
@@ -24,10 +27,29 @@ public class HistoryFragmentViewModel extends ViewModel {
     }
 
     public void deleteAll() {
-        dao.clearSessions();
+        new clearSessionAsyncTask().execute();
+//        dao.clearSessions();
     }
 
     public void deleteSingle(Session s) {
-        dao.deleteSession(s);
+        //dao.deleteSession(s);
+        new deleteSessionAsyncTask().execute(new Session[]{s});
+    }
+
+    private class clearSessionAsyncTask extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
+            dao.clearSessions();
+            return null;
+        }
+    }
+
+    private class deleteSessionAsyncTask extends AsyncTask<Session, Void, Void>{
+        @Override
+        protected Void doInBackground(Session... sessions) {
+            dao.deleteSession(sessions[0]);
+            return null;
+        }
+
     }
 }
